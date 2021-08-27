6a24baea20b34ad8ba3eb34dd9e148cbec6f83e3
jaherlihy <jaherlihy@gmail.com>
2021-08-26 19:57:24 -0500

    got the scoreboard working but now I'm unsure if I want to set it up on a different html file or try to make it work within the same page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,38 @@
 var intro = document.getElementById("intro-title-card");
 var game = document.getElementById("game");
 var startButton = document.getElementById("begin");
+var scoreBtn = document.getElementById("high-scores-btn");
 var outro = document.getElementById("outro");
-// var textBox = 
 
 var question = document.getElementById("question");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
-
 var right = document.getElementById("correct");
 var wrong = document.getElementById("incorrect");
 
 var timerElement = document.querySelector(".timer-count");
 
-var submitInitals
-var initials
-var scoreListing
+var scrbrdForm = document.getElementById("score-board-entry");
+var textBox = document.getElementById("text-box");
+var ol = document.getElementById("score-board");
+var li = document.createElement("li");
+var submitBtn = document.getElementById("submit-btn");
+var clearBtn = document.getElementById("clear-btn");
+var score = "";
 
+// Check for saved scores
+var savedInitals = localStorage.getItem("initialItems");
 
-
-
+// Updates the scoreboard if there are any saved scores
+if (savedInitals) {
+	ol.textContent = savedInitals;
+}
 
 var questions = [
 	{
-		question: "Which global object is held by square brackets []?",
+		question: "Which global object is held by square brackets?",
 		choiceA: "Console.log",
 		choiceB: "Array",
 		choiceC: "For loop",
@@ -47,10 +54,10 @@ var questions = [
 		correct: "A"
 	},{
 		question: "Which function returns the largest integer less than or equal to a given number?",
-		choiceA: "Math.round()",
-		choiceB: "Math.ceil()",
-		choiceC: "Math.floor()",
-		choiceD: "Math.abs()",
+		choiceA: "Math.round( )",
+		choiceB: "Math.ceil( )",
+		choiceC: "Math.floor( )",
+		choiceD: "Math.abs( )",
 		correct: "C"
 	}
 ];
@@ -58,6 +65,10 @@ var questions = [
 var prevQuestion = questions.length -1;
 var currentQuestion = 0;
 
+// function init() {
+// 	scoreBtn.disable = false;
+// 	startButton.disable = false;
+// }
 
 function questionDisplay() {
 	let q = questions[currentQuestion];
@@ -75,7 +86,7 @@ function questionDisplay() {
 startButton.addEventListener("click", startGame); 
 
 function startGame() {
-	console.log("game started");
+	console.log("game has started");
 	intro.style.display = "none";
 	questionDisplay();
 	game.style.display = "block";
@@ -83,9 +94,8 @@ function startGame() {
 	timerCount = 60;
 	right.hidden = true;
 	wrong.hidden = true;
+	scoreBtn.disable = true;
   }
-
-
 
   function answerVerify(answer){
 	if(answer == questions[currentQuestion].correct){
@@ -109,7 +119,7 @@ function startGame() {
 		
 		clearInterval(timer);
 		console.log("all done!");
-		var score = timerCount
+		score = timerCount
 		console.log("your score is: " + score);
 		setTimeout (gameWin, 1000);
 		
@@ -136,10 +146,37 @@ function gameWin () {
 	right.hidden = true;
 	wrong.hidden = true;
 	game.style.display = "none";
+	scoreBtn.disable = true;
 	outro.style.display = "block";
 
-
 }
+
+scrbrdForm.addEventListener("submit", function (event) {
+
+		// stops the form from submitting
+		event.preventDefault();
+
+		// Ignore if the name is too long or short
+		if (textBox.value.length < 1 || textBox.value.length > 10) return;
+
+		ol.appendChild(li)
+		// Add name to the scoreboard
+		li.textContent += textBox.value + " - " + score;
+
+		
+
+		// Clear input after submitting
+		textBox.value = "";
+
+		// Save the list to localStorage
+		localStorage.setItem("initialsItems", textBox.textContent);
+
+		//prevent multiple entries by disabling button
+		submitBtn.disable = true;
+
+	}, false);
+
+
 
 
 function startTimer() { 
